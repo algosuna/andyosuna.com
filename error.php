baa0f8270e3b6b4d82ca0a1d7adbff4536b1d101
andyosuna <andyosuna@gmail.com>
2014-11-27 13:29:48 -0800

    computer hiccup

--- error.php
+++ error.php
@@ -1,7 +1,7 @@
 <div class="content container">
-	<h2><?php echo $errorTitle; ?></h2>
-	<div class="error-gif">
-		<img src="images/<?php echo $pageCurrent; ?>.gif" alt="<?php echo $errorTitle; ?>">
-	</div>
-	<p><?php echo $error; ?></p>
+  <h2><?php echo $errorTitle; ?></h2>
+  <div class="error-gif">
+    <img src="images/<?php echo $pageCurrent; ?>.gif" alt="<?php echo $errorTitle; ?>">
+  </div>
+  <p><?php echo $error; ?></p>
 </div>
