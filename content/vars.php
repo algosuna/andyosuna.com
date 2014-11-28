e164d62ba69e42c2135adea5d3f5b808cee8118b
andyosuna <andyosuna@gmail.com>
2014-11-28 15:15:52 -0800

    php5 weirdness
    added a function to settle down php5 and added a todo list with one item

--- content/vars.php
+++ content/vars.php
@@ -8,6 +8,8 @@
 // if the user changes the language manually
 // then a cookie is set to remember for a month
 
+date_default_timezone_set('UTC');
+
 if (isset($_GET['lang'])):
 	$lang = $_GET['lang'];
 	setcookie('lang', $lang, strtotime('+30 days'));
