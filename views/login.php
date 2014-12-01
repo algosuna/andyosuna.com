5b0036f5a08ad7a644aa3b3d9ba1a06375160df9
andyosuna <andyosuna@gmail.com>
2014-11-30 22:35:25 -0800

    added login view
    basic form that goes nowhere

---
+++ views/login.php
@@ -0,0 +1,12 @@
+<div class="content container">
+  <h2>Login</h2>
+  <div class="login-container">
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" name="login">
+      <label for="username">Username:</label>
+      <input type="text" name="username" placeholder="awesome" value="">
+      <label for="password">Password:</label>
+      <input type="password" name="password" value="">
+      <button type="submit" class="btn">Log In</button>
+    </form>
+  </div>
+</div>
